6b2e6e1f898eb416468992d825021d469ffe72eb
Hmace52 <72214542+Hmace52@users.noreply.github.com>
2020-11-08 21:08:29 +0000

    Update Steam_API.py
    Fixing the gameList

--- Discord chatbot/Data_Control_Files/Steam_API.py
+++ Discord chatbot/Data_Control_Files/Steam_API.py
@@ -6,7 +6,7 @@ from Steam_API import steamHandler
 steamHandler.gamePlayerCount(730)
 """
 
-import urllib.request, json
+import urllib.request, json, os
 
 
 class steam_APIM:
@@ -44,8 +44,11 @@ class steam_APIM:
         :return: steamID of given game as a string or False if the ID was not found
         """
         gameID = False
+        # Generates a file path independent from the calling program
+        filePath = os.path.dirname(__file__)
+        gameListFilePath = os.path.join(filePath, "gameList.json")
         # Searches json file for the data related to the game name provided
-        with open('gameList.json',) as f:
+        with open(gameListFilePath,) as f:
             gameListData = json.load(f)
         for gameData in gameListData["applist"]["apps"]:
             if gameData["name"].lower() == gameName.lower():
