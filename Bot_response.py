bba21c931012f20809ca1f5ff5cb77bf605d34ac
ralt123 <70897677+ralt123@users.noreply.github.com>
2020-11-25 13:29:54 +0000

    Update Bot_response.py

--- Bot_response.py
+++ Bot_response.py
@@ -1,14 +1,15 @@
+import nltk
+nltk.download()
 import re
 from nltk.corpus import wordnet
 
-listofwords=['hello','Streamer']
+
+listofwords=['hello','Streamer', 'info', 'clear' ,'kick', 'ban','game' ,'commands','friends']
 listofsynonym={}
 keywords={}
 keywordsdictonary={}
-keywords['greet'] = []
-responses={ 'greet':'Hello! How can I help you?',
-    'Streamer':'your favourite streamers are ...',
-    'Undefined':'I dont quite understand. Could you repeat that?'}
+
+
 
 for word in listofwords:
   synonyms=[]
@@ -16,21 +17,54 @@ for word in listofwords:
     for x in synonym.lemmas():
       name = re.sub('[^a-zA-Z0-9 \n\.]', ' ', x.name())
       synonyms.append(name)
-
   listofsynonym[word]=set(synonyms)
 
+keywords['greet']=[]
 for synonym in list(listofsynonym['hello']):
   keywords['greet'].append('.*\\b'+synonym+'\\b.*')
-
 keywords['Streamer']=[]
 for synonym in list(listofsynonym['Streamer']):
   keywords['Streamer'].append('.*\\b'+synonym+'\\b.*')
+keywords['info']=[]
+for synonym in list(listofsynonym['info']):
+  keywords['info'].append('.*\\b'+synonym+'\\b.*')
+keywords['clear']=[]
+for synonym in list(listofsynonym['clear']):
+  keywords['clear'].append('.*\\b'+synonym+'\\b.*')
+keywords['kick']=[]
+for synonym in list(listofsynonym['kick']):
+  keywords['kick'].append('.*\\b'+synonym+'\\b.*')
+keywords['ban']=[]
+for synonym in list(listofsynonym['ban']):
+  keywords['ban'].append('.*\\b'+synonym+'\\b.*')
+keywords['game']=[]
+for synonym in list(listofsynonym['game']):
+  keywords['game'].append('.*\\b'+synonym+'\\b.*')
+keywords['commands']=[]
+for synonym in list(listofsynonym['commands']):
+  keywords['commands'].append('.*\\b'+synonym+'\\b.*')
+keywords['friends']=[]
+for synonym in list(listofsynonym['friends']):
+  keywords['friends'].append('.*\\b'+synonym+'\\b.*')
+
+
+responses={ 
+'greet':bruh,
+'Streamer':function,
+'info':function,
+'clear' :function,
+'kick':function,
+'ban':function,
+'commands':function,
+'game' :function,
+'friends':function,
+'Undefined':function
+}
+
 for intent, keys in keywords.items():
   keywordsdictonary[intent]=re.compile('|'.join(keys))
-
-
-def output(input):
-  userinput = input.lower()
+while True:
+  userinput = input().lower()
   matchedintent = None 
   for intent,pattern in keywordsdictonary.items():
     if re.search(pattern, userinput):
@@ -38,4 +72,10 @@ def output(input):
   key='Undefined' 
   if matchedintent in responses:
     key = matchedintent
-  return responses[key]
+  responses[key]()
+
+
+
+
+
+
